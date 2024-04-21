81cbf6d6bb5f081cf9b928eef82efe207b3fb388
Roni Febrone de Almeida <132415797+RoniFebrone@users.noreply.github.com>
2024-04-21 01:16:23 -0300

    add Selects mais instrucao

---
+++ insomniaAndCommand/verififyTables.sql
@@ -0,0 +1,36 @@
+select * from estoque e ; 			-- TUDO OK PARA ESTOQUE,
+										-- LISTA ITENS DO ESTOQUE
+										-- LISTA UM ITEM DO ESTOQUE
+										-- EDITA QUANTIDADE EM ESTOQUE 
+										-- EXCLUI ITEM DO ESTOQUE.
+
+select * from produtos p ;		 	-- TUDO OK PARA PRODUTOS,
+										-- LISTA PRODUTOS
+										-- LISTA UM PRODUTOS
+										-- CRIA PRODUTO
+										-- EDITA PRODUTO 
+										-- EXCLUI PRODUTO.
+
+select * from pedido_produto pp ;	-- TUDO OK PARA PERDIDO_PRODUTO
+                                        -- LISTA ITENS DO PEDIDO
+
+
+select * from pedidos p;  			-- TUDO OK PARA PEDIDOS,
+										-- LISTA PEDIDOS
+										-- LISTA UM PEDIDOS
+										-- EDITAR PEDIDOS 
+										-- EXCLUI PEDIDOS
+
+select * from clientes c ; 			-- TUDO OK PARA CLIENTES,
+										-- LISTA CLIENTES
+										-- LISTA UM CLIENTES
+										-- EDITA CLIENTES 
+										-- EXCLUI CLIENTES.
+
+select * from vendas; 				-- TUDO OK PARA CLIENTES,
+										-- LISTA VENDAS
+										-- LISTA UMA VENDA
+										-- EXCLUI VENDA
+
+
+
